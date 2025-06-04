280744fdc7d7422bc4b9a2458ef2774fe04508f0
kandylove <kandywebberlove@ischool.berkeley.edu>
2025-06-04 19:58:57 -0400

    Update app.py
    trying api update againi

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify  
 app = Flask(__name__)
 
 @app.route('/')
 def w209():
-    file='about9.jpg'
-    return render_template('w209.html',file=file)
-    
-@app.route('/api')  
+    file = 'about9.jpg'
+    return render_template('w209.html', file=file)
+
+@app.route('/api') 
 def api():
     return jsonify(x=42)
-    
+
 if __name__ == '__main__':
-    app.run()
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
